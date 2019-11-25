87dc93edd48fb2365d03985ff9f7fdefe625d0ed
avina007 <avina007@cougars.csusm.edu>
2019-11-24 20:28:48 -0800

    added reservation class and removed useless progress bar that gave us unwanted alert

---
+++ PawrkingSample/PawrkingSample/Classes/Reservation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PawrkingSample.Classes
+{
+    class Reservation
+    {
+        ParkingLot space { set; get; }
+        string StudentID { set; get; }
+        int timer { set; get; }
+    }
+}
